a3c202c62abf701b11a455fb0a9a41dc803f409c
Azhar Shah <azharshahsaintlouis@gmail.com>
2024-11-17 15:01:29 +0000

    add Auth comp

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,29 @@
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 const Auth = () => {
+
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const signIn = async () =>  {
+        await createUserWithEmailAndPassword();
+    }
+
+
     return (
         <>
-        <input placeholder="email" />
-        <input placeholder="password" />
-        <button>Sign in</button>       
+        <input onChange={ handleEmailChange } placeholder="email" />
+        <input onChange={ handlePasswordChange } placeholder="password" />
+        <button onClick = { signIn }>Sign in</button>       
         </>
     )
 }
