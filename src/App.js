09b559ea1539024d166e1920605998f42bc22e04
Azhar Shah <azharshahsaintlouis@gmail.com>
2025-01-27 16:09:26 +0000

    add ui for recipes

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
     const getRecipes = async () => {
       try {
         const recipeData = await getDocs(recipesCollectionRef);
-        const filteredData = recipeData.docs.map((doc) => ({
+        const filteredRecipes = recipeData.docs.map((doc) => ({
           ...doc.data(),
           id: doc.id,
         }));
 
-        console.log("recipe data", filteredData);
+        setRecipes(filteredRecipes);
       } catch (err) {
         console.log(err);
       }
@@ -30,6 +30,33 @@ function App() {
   return (
     <div className="App">
       <Auth />
+
+      <div>
+        <label htmlFor="title">Title</label>
+        <input placeholder="title" id="title" />
+        <label placeholder="difficulty selector" htmlFor="difficulty">
+          Choose the difficulty level:
+        </label>
+        <select name="ratings" id="difficulty selector">
+          <option value="">-- Please choose a difficulty rating --</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="3">4</option>
+        </select>
+      </div>
+      <div>
+        {recipes.map((recipe) => {
+          return (
+            <div key={recipe.id}>
+              <h1>Title: {recipe.title}</h1>
+              <h3>Ingredients: {recipe.ingredients}</h3>
+              <h3>Difficulty Rating: {recipe.difficultyRating}</h3>
+              <h3>Directions: {recipe.directions}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
